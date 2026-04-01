c942087b4c37e6beda0b67963077875afd097cfa
kanakchhabra4674stell-glitch <kanakchhabra4674stell@gmail.com>
2026-04-01 08:55:23 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,11 @@
+function show(){
+            let name=document.getElementsByName("myName").value;
+            //document.getElementById("display").innerHTML=name;
+            alert(name);
+
+        }
+        //dom on 11th march
+        function colorChange() {
+            document.getElementById
+
+        }
