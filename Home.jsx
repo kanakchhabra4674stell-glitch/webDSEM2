9666bd3f10e9e02d1d17e2654cdeadf277005ac9
kanakchhabra4674stell-glitch <kanakchhabra4674stell@gmail.com>
2026-05-16 22:23:29 +0530

    Home web page

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function Home() {
+    const navigate=useNavigate()
+  return (
+    <div>
+        <h1>Home</h1>
+        <button onClick={()=>navigate('/cart')}>Go To Cart</button>
+    </div>
+  )
+}
+
+export default Home
